a5ad64af82f884580f37410fda779289aa14315b
wellington <wellingtonslpbi@hotmail.com>
2021-01-27 09:10:39 -0300

    Add ThemeProvider and styled-components.d.ts

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,14 +1,16 @@
 import { addDecorator } from '@storybook/react'
 import { withNextRouter } from 'storybook-addon-next-router'
 import GlobalStyles from '../src/styles/global'
+import theme from '../src/styles/theme'
+import { ThemeProvider } from 'styled-components'
 
 addDecorator(withNextRouter())
 
 export const decorators = [
   (Story) => (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyles />
       <Story />
-    </>
+    </ThemeProvider>
   )
 ]
