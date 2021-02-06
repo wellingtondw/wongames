a29828f50d5c7f17926ba2061567b9cb6619104d
wellington <wellingtonslpbi@hotmail.com>
2021-02-05 21:25:28 -0300

    Add Button with sizes

---
+++ client/src/components/Button/test.tsx
@@ -0,0 +1,9 @@
+// import { render, screen } from '@testing-library/react'
+
+// import Button from '.'
+
+// describe('<Button />', () => {
+//   it('should render the heading', () => {
+//     const { container } = render(<Button />)
+//   })
+// })
