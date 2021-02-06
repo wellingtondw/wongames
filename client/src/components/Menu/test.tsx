be0437fbd7bdbf1f7144e57b8a13b2ea1a82df75
wellington <wellingtonslpbi@hotmail.com>
2021-02-06 11:06:20 -0300

    Add logic open/close to menu

--- client/src/components/Menu/test.tsx
+++ client/src/components/Menu/test.tsx
@@ -1,14 +1,35 @@
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 import Menu from '.'
 
 describe('<Menu />', () => {
   it('should render the menu', () => {
     renderWithTheme(<Menu />)
-    
+
     expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
     expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
     expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
     expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
+  })
+
+  it('should handle the open/close mobile menu', () => {
+    renderWithTheme(<Menu />)
+
+    // select our MenuFull
+    const fullMenuElement = screen.getByRole('navigation', { hidden: true })
+
+    // check if the menu is hidden
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
+
+    // click on the button to open the menu and check if it opened
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
+    expect(fullMenuElement).toHaveStyle({ opacity: 1 })
+
+    // click on the button to close the menu and check if it closed
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
+    expect(fullMenuElement).toHaveStyle({ opacity: 0 })
   })
 })
