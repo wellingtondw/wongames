64755dde936d5d374a92ed92fabd091c3a887510
wellington <wellingtonslpbi@hotmail.com>
2021-02-05 21:43:17 -0300

    Add Button support for icons

--- client/src/components/Button/stories.tsx
+++ client/src/components/Button/stories.tsx
@@ -1,5 +1,6 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Button from '.'
+import { AddShoppingCart } from '@styled-icons/material-outlined/AddShoppingCart'
 
 export default {
   title: 'Button',
@@ -7,6 +8,9 @@ export default {
   argTypes: {
     children: {
       type: 'string'
+    },
+    icon: {
+      type: ''
     }
   }
 } as Meta
@@ -15,4 +19,12 @@ export const Default: Story = (args) => <Button {...args} />
 
 Default.args = {
   children: 'Buy now'
+}
+
+export const withIcon: Story = (args) => <Button {...args} />
+
+withIcon.args = {
+  size: 'small',
+  children: 'Buy now',
+  icon: <AddShoppingCart />
 }
