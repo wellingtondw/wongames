b661c70d02b805aa0e258fc2f4b13f8c7c12bac9
wellington <wellingtonslpbi@hotmail.com>
2021-02-11 09:28:53 -0300

    Add highlight initial styles

--- client/src/components/Highlight/stories.tsx
+++ client/src/components/Highlight/stories.tsx
@@ -7,8 +7,12 @@ export default {
   args: {
     title: 'Read Dead it’s back',
     subtitle: 'Come see John’s new adventures',
+    backgroundImage: '/img/red-dead-img.jpg',
     buttonLabel: 'Buy now',
     buttonLink: '/rdr2'
+  },
+  parameters: {
+    layout: 'fullscreen'
   }
 } as Meta
 
