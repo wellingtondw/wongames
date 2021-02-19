8f496d87fab43ac8301bef7f9e961b26d491245b
wellington <wellingtonslpbi@hotmail.com>
2021-02-19 10:19:41 -0300

    Add getServerSideProps method and create mocks

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,5 +1,21 @@
-import Home from 'templates/Home'
+import Home, { HomeTemplateProps } from 'templates/Home'
 
-export default function Index() {
-  return <Home />
+export default function Index({ ...props }: HomeTemplateProps) {
+  return <Home {...props} />
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      banners: [],
+      newGames: [],
+      mostPopularHighlight: {},
+      mostPopularGames: [],
+      upcommingGames: [],
+      upcommingHighlight: {},
+      upcommingMoreGames: [],
+      freeGames: [],
+      freeHighlight: {}
+    }
+  }
 }
--- client/src/templates/Home/index.tsx
+++ client/src/templates/Home/index.tsx
@@ -1,37 +1,78 @@
+import { BannerProps } from 'components/Banner'
+import { GameCardProps } from 'components/GameCard'
+import Highlight, { HighlightProps } from 'components/Highlight'
+
 import { Container } from 'components/Container'
 import Footer from 'components/Footer'
 import Heading from 'components/Heading'
 import Menu from 'components/Menu'
+import GameCardSlider from 'components/GameCardSlider'
+import BannerSlider from 'components/BannerSlider'
 
-const Home = () => {
+export type HomeTemplateProps = {
+  banners: BannerProps[]
+  newGames: GameCardProps[]
+  mostPopularHighlight: HighlightProps
+  mostPopularGames: GameCardProps[]
+  upcommingGames: GameCardProps[]
+  upcommingHighlight: HighlightProps
+  upcommingMoreGames: GameCardProps[]
+  freeGames: GameCardProps[]
+  freeHighlight: HighlightProps
+}
+
+const Home = ({
+  banners,
+  newGames,
+  mostPopularHighlight,
+  mostPopularGames,
+  upcommingGames,
+  upcommingHighlight,
+  upcommingMoreGames,
+  freeGames,
+  freeHighlight
+}: HomeTemplateProps) => {
   return (
     <section>
       <Container>
         <Menu />
+        <BannerSlider items={banners} />
       </Container>
 
       <Container>
         <Heading lineLeft lineColor="secondary" color="black">
           News
         </Heading>
+
+        <GameCardSlider items={newGames} color="black" />
       </Container>
 
       <Container>
         <Heading lineLeft lineColor="secondary">
           Most Popular
         </Heading>
+
+        <Highlight {...mostPopularHighlight} />
+        <GameCardSlider items={mostPopularGames} />
       </Container>
 
       <Container>
         <Heading lineLeft lineColor="secondary">
           Upcomming
         </Heading>
+
+        <GameCardSlider items={upcommingGames} />
+        <Highlight {...upcommingHighlight} />
+        <GameCardSlider items={upcommingMoreGames} />
       </Container>
 
       <Container>
         <Heading lineLeft lineColor="secondary">
           Free Games
         </Heading>
+
+        <Highlight {...freeHighlight} />
+        <GameCardSlider items={freeGames} />
       </Container>
 
       <Container>
