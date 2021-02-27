2b3b93f5554dd010ee4a1b735a84841b482460b4
wellington <wellingtonslpbi@hotmail.com>
2021-02-27 08:46:44 -0300

    Add disabled option to TextField

--- client/src/components/TextField/test.tsx
+++ client/src/components/TextField/test.tsx
@@ -72,4 +72,45 @@ describe('<TextField />', () => {
     userEvent.tab()
     expect(input).toHaveFocus()
   })
+
+  it('Does not changes its value when disabled', async () => {
+    const onInput = jest.fn()
+    renderWithTheme(
+      <TextField
+        onInput={onInput}
+        label="TextField"
+        labelFor="TextField"
+        id="TextField"
+        disabled
+      />
+    )
+
+    const input = screen.getByRole('textbox')
+    expect(input).toBeDisabled()
+
+    const text = 'This is my new text'
+    userEvent.type(input, text)
+
+    await waitFor(() => {
+      expect(input).not.toHaveValue(text)
+    })
+    expect(onInput).not.toHaveBeenCalled()
+  })
+
+  it('Is not accessible by tab when disabled', () => {
+    renderWithTheme(
+      <TextField
+        label="TextField"
+        labelFor="TextField"
+        id="TextField"
+        disabled
+      />
+    )
+
+    const input = screen.getByLabelText('TextField')
+    expect(document.body).toHaveFocus()
+
+    userEvent.tab()
+    expect(input).not.toHaveFocus()
+  })
 })
