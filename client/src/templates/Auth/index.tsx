795e356a52dd5dfba658f4886a753571e3611d99
wellington <wellingtonslpbi@hotmail.com>
2021-03-01 07:49:46 -0300

    create first auth structure with tests

--- client/src/templates/Auth/index.tsx
+++ client/src/templates/Auth/index.tsx
@@ -1,8 +1,34 @@
+import Heading from 'components/Heading'
+import Logo from 'components/Logo'
+import React from 'react'
 import * as S from './styles'
 
-const Auth = () => (
+type AuthProps = {
+  title: string
+  children: React.ReactNode
+}
+
+const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
-    <h1>Auth</h1>
+    <S.BannerBlock>
+      <Logo />
+
+      <Heading>All your favorites games in one place</Heading>
+      <S.Subtitle>
+        <strong>won</strong> is the best and most complete gaming platform
+      </S.Subtitle>
+
+      <S.Footer>Won Games 2020 © All rights reserved</S.Footer>
+    </S.BannerBlock>
+
+    <S.Content>
+      <Logo size="large" color="black" />
+      <Heading color="black" lineColor="secondary" lineLeft>
+        {title}
+      </Heading>
+
+      {children}
+    </S.Content>
   </S.Wrapper>
 )
 
--- client/src/templates/Auth/test.tsx
+++ client/src/templates/Auth/test.tsx
@@ -1,13 +1,27 @@
-import { render, screen } from '@testing-library/react'
-
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 import Auth from '.'
 
 describe('<Auth />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Auth />)
+  it('should render logos, title, subtitle, children', () => {
+    const { container } = renderWithTheme(
+      <Auth title="Auth title">
+        <input type="text" />
+      </Auth>
+    )
 
-    expect(screen.getByRole('heading', { name: /Auth/i })).toBeInTheDocument()
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: /Auth title/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best and most complete gaming platform/i
+      })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
   })
 })
