38debb2ccc268d9c9ec7a1d389d112aab9d40ecb
wellington <wellingtonslpbi@hotmail.com>
2021-03-01 20:14:37 -0300

    Add Auth Desktop style

--- client/src/templates/Auth/index.tsx
+++ client/src/templates/Auth/index.tsx
@@ -11,23 +11,29 @@ type AuthProps = {
 const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
-      <Logo />
+      <S.BannerContent>
+        <Logo />
 
-      <Heading>All your favorites games in one place</Heading>
-      <S.Subtitle>
-        <strong>won</strong> is the best and most complete gaming platform
-      </S.Subtitle>
+        <div>
+          <Heading>All your favorites games in one place</Heading>
+          <S.Subtitle>
+            <strong>won</strong> is the best and most complete gaming platform
+          </S.Subtitle>
+        </div>
 
-      <S.Footer>Won Games 2020 © All rights reserved</S.Footer>
+        <S.Footer>Won Games 2020 © All rights reserved</S.Footer>
+      </S.BannerContent>
     </S.BannerBlock>
 
     <S.Content>
-      <Logo size="large" color="black" />
-      <Heading color="black" lineColor="secondary" lineLeft>
-        {title}
-      </Heading>
+      <S.ContentWrapper>
+        <Logo size="large" color="black" />
+        <Heading color="black" lineColor="secondary" lineLeft>
+          {title}
+        </Heading>
 
-      {children}
+        {children}
+      </S.ContentWrapper>
     </S.Content>
   </S.Wrapper>
 )
