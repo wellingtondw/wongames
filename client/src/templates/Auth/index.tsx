cb3820d1259a38a8e5007a8059c86a7ebb5e9aee
wellington <wellingtonslpbi@hotmail.com>
2021-03-01 21:27:40 -0300

    Fix Logo id and def

--- client/src/templates/Auth/index.tsx
+++ client/src/templates/Auth/index.tsx
@@ -12,7 +12,7 @@ const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
       <S.BannerContent>
-        <Logo />
+        <Logo id="banner" />
 
         <div>
           <Heading>All your favorites games in one place</Heading>
@@ -27,7 +27,7 @@ const Auth = ({ title, children }: AuthProps) => (
 
     <S.Content>
       <S.ContentWrapper>
-        <Logo size="large" color="black" />
+        <Logo id="content" color="black" size="large" />
         <Heading color="black" lineColor="secondary" lineLeft>
           {title}
         </Heading>
