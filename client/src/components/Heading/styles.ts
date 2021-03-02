548112999167c50c283d9bcd60714c57a59c71db
wellington <wellingtonslpbi@hotmail.com>
2021-03-01 21:56:03 -0300

    Add new huge size to Heading

--- client/src/components/Heading/styles.ts
+++ client/src/components/Heading/styles.ts
@@ -10,18 +10,19 @@ const wrapperModifiers = {
       width: 3rem;
     }
   `,
-
   medium: (theme: DefaultTheme) => css`
     font-size: ${theme.font.sizes.xlarge};
     ${media.greaterThan('medium')`
       font-size: ${theme.font.sizes.xxlarge};
     `}
   `,
+  huge: (theme: DefaultTheme) => css`
+    font-size: ${theme.font.sizes.huge};
+  `,
   lineLeft: (theme: DefaultTheme, lineColor: LineColors) => css`
     padding-left: ${theme.spacings.xxsmall};
     border-left: 0.7rem solid ${theme.colors[lineColor]};
   `,
-
   lineBottom: (theme: DefaultTheme, lineColor: LineColors) => css`
     position: relative;
     margin-bottom: ${theme.spacings.medium};
@@ -40,9 +41,7 @@ export const Wrapper = styled.h2<HeadingProps>`
   ${({ theme, color, lineLeft, lineBottom, lineColor, size }) => css`
     color: ${theme.colors[color!]};
     font-size: ${theme.font.sizes.xlarge};
-    ${media.greaterThan('medium')`
-      font-size: ${theme.font.sizes.xxlarge};
-    `}
+
     ${lineLeft && wrapperModifiers.lineLeft(theme, lineColor!)}
     ${lineBottom && wrapperModifiers.lineBottom(theme, lineColor!)}
     ${!!size && wrapperModifiers[size](theme)}
